b503bc5b70c4cf8fa3e9244bba29b59217914603
BeedPro <annoor28@hotmail.co.uk>
2024-05-15 23:20:32 +0100

    Added an Edit function to Category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,8 +14,49 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            return categoryFromDb is null ? NotFound() : View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _ = _db.Categories.Update(obj);
+                _ = _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(
+                    "name",
+                    "The Display Order cannot exactly match the Category Name"
+                );
+            }
+            if (ModelState.IsValid)
+            {
+                _ = _db.Categories.Add(obj);
+                _ = _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
